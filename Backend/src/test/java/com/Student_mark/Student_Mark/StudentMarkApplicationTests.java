639adddc08fc804429f5ab457a88b61adc710401
RAGAV-24 <ragavrunner33@gmail.com>
2025-05-01 14:46:07 +0530

    started my project

---
+++ Backend/src/test/java/com/Student_mark/Student_Mark/StudentMarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Student_mark.Student_Mark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
